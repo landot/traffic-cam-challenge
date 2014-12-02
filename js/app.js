54fb1cbc5f632574c19161cb8821f520daa1a1b4
Tim <timowland@gmail.com>
2014-12-02 01:04:48 -0800

    need to get markers to work

--- js/app.js
+++ js/app.js
@@ -14,20 +14,44 @@
 $(document).ready(function() {
 	var mapElem = document.getElementById('map');
 
-	function createMap(elem, center, zoom) {
-		var map= new google.maps.Map(elem, {
+		var map= new google.maps.Map(mapElem, {
 			center: center,
-			zoom: zoom,
+			zoom: 12,
 			mapTypeId: google.maps.MapTypeId.ROADMAP
 		});
 		var infoWindow = new google.maps.InfoWindow();
-		infoWindow.setContent("");
-	}
+	
 	var center = {
 		lat: 47.6,
 		lng: -122.3
 	};	
 
-	createMap(mapElem, center, 12);
-	
+	var cameras;
+	var markers = [];
+
+	$.getJSON('http://data.seattle.gov/resource/65fc-btcc.json')
+		.done(function(data) {
+			cameras = data;
+			data.forEach(function(camera) {
+				var marker = new google.maps.Marker({
+					position: {
+						lat: Number(camera.location.latitude),
+						lng: Number(camera.location.longitude)
+					}
+					map: map
+				});
+				markers.push(marker);
+
+                google.maps.event.addListener(marker, 'click', function() {
+	                var html = "<h3>" + camera.cameralabel + "</h3>";
+	                html+=
+	                infoWindow.setContent(html);
+	                infoWindow.open(map, this);    
+	            });
+            });            
+		})
+
+
+
+
 });
